cdc304dd77108c78e332b785af849e5526cf3b8a
stressart <stressamail@gmail.com>
2013-07-03 13:59:29 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,28 +6,30 @@
 
 
 def main():
-	import socket			#импортируем модуль "soket"
-	host="localhost"		#здесь должны указывать IP
-	port=6000				#порт
-	bufsize=1024			#размер получаемого/отсылаемого пакета
-	s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#создаем беркли сокет для tcp
-	if s==-1:
+	import socket		
+	host = "localhost"		#здесь должны указывать IP
+	port = 6000			
+	bufsize = 1024			#размер получаемого/отсылаемого пакета
+
+	s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )	#создаем беркли сокет для tcp
+	if s == -1:
 		print 'socket not created'
-	s.bind((host,port))		#биндим ip:port чтобы не вмешались другие программы
-	s.listen(2)				#слушаем 1 соединение
 	
-	while True:				#бесконечный цикл пока не break'нем
+	s.bind( ( host,port ) )		#биндим ip:port чтобы не вмешались другие программы
+	s.listen( 2 )				#слушаем 1 соединение
+	
+	while True:				
 		print 'waiting for connection...'
-		clientsock,addr=s.accept()		#получаем клиентский сокет?? и адрес, переводим сервер в режим ожидания подключения accept()
+		clientsock,addr = s.accept()		#получаем клиентский сокет?? и адрес, переводим сервер в режим ожидания подключения accept()
 		print '...connected from: ',addr
 		
-		while True:			#еще 1 бесконечный цикл
-			mdata=clientsock.recv(bufsize)	#получаем данные
+		while True:			
+			mdata = clientsock.recv(bufsize)	#получаем данные
 			if not mdata:	#если нет данных - прерываем цикл
 				print 'client ',addr,' disconnected'
 				break
-			print mdata		#печатаю данные на сервере
-			clientsock.send(mdata) #посылаем принятые данные обратно
+			print mdata	
+			clientsock.send( mdata ) #посылаем принятые данные обратно
 			
 			
 		clientsock.close()	#закрываем клиентский сокет??
