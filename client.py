ac010d721fdc905b2a3612d4e792342bef2317cd
stressart <stressamail@gmail.com>
2013-07-03 13:49:43 +0300

    Update client.py

--- client.py
+++ client.py
@@ -4,20 +4,20 @@
 
 def main():
 	import socket		
-	host='localhost'
-	port=6000
-	bufsize=1024
+	host = 'localhost'
+	port = 6000
+	bufsize = 1024
 	
-	clientsock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	clientsock.connect((host,port))
+	clientsoc k= socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+	clientsock.connect( (host,port) )
 		
 	while True:
-		mdata=raw_input('> ')	#получаем строки с клавиатуры
-		if mdata=='exit':		#для выхода
+		mdata = raw_input('> ')	#получаем строки с клавиатуры
+		if mdata == 'exit':		#для выхода
 			print 'bye bye'
 			break
 		clientsock.send(mdata)
-		mdata=clientsock.recv(bufsize)
+		mdata = clientsock.recv( bufsize )
 		print mdata				#печатаем получаемые данные
 			
 	clientsock.close()
